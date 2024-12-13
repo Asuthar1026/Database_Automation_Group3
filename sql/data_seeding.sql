ef957b481f8af1a1c55b23ce6180467ee7552530
Astha <asthasuthar888@gmail.com>
2024-12-12 22:20:22 -0500

    pushing sql files

---
+++ sql/data_seeding.sql
@@ -0,0 +1,7 @@
+INSERT INTO ClimateData (location, record_date, temperature, precipitation, humidity)
+VALUES
+    ('Ahmedabad', '2023-01-01', 5.6, 10.2, 72.0),
+    ('Mehsana', '2023-02-04', 20.4, 0.8, 55.0),
+    ('Siddhpur', '2023-03-08', -2.0, 15.0, 80.0),
+    ('Palanpur', '2023-04-06', 28.6, 0.9, 65.0),
+    ('Surat', '2023-05-08', -4.0, 15.0, 90.0);
